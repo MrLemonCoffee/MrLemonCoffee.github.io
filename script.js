333cefba5ed1b4dd808cdcb8631ca5f8be5b6508
MrLemonCoffee <grzkry1@wp.pl>
2022-05-06 17:21:19 +0200

    Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://kodilla-tasks-69.herokuapp.com/v1/tasks';
+  const apiRoot = 'https://kodilla-tasks-69.herokuapp.com/v1/task';
   const trelloApiRoot = 'https://kodilla-tasks-69.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
